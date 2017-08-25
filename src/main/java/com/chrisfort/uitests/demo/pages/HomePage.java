1f4a67e9aa6eea714f4eb3b6115c152ba98cca2b
cpfort <chris.fort@teamaol.com>
2017-08-25 14:10:20 -0400

    bug fixes and added a sample test

---
+++ src/main/java/com/chrisfort/uitests/demo/pages/HomePage.java
@@ -0,0 +1,15 @@
+package com.chrisfort.uitests.demo.com.chrisfort.uitests.demo.pages;
+
+import com.chrisfort.uitests.demo.utilities.AbstractPage;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by cpfort on 8/25/17.
+ */
+public class HomePage extends AbstractPage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
---
+++ src/main/java/com/chrisfort/uitests/demo/tests/HomePageTest.java
@@ -0,0 +1,7 @@
+package com.chrisfort.uitests.demo.tests;
+
+/**
+ * Created by cpfort on 8/25/17.
+ */
+public class HomePageTest {
+}
