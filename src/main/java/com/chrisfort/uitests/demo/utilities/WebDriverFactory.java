fc9d64d08b9609aa882b2c57765167e2570cf235
cpfort <chris.fort@teamaol.com>
2017-09-08 08:26:51 -0400

    Added Apple iPhone 6 as emulated driver (Chrome)

--- src/main/java/com/chrisfort/uitests/demo/utilities/WebDriverFactory.java
+++ src/main/java/com/chrisfort/uitests/demo/utilities/WebDriverFactory.java
@@ -2,6 +2,7 @@ package com.chrisfort.uitests.demo.utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -10,19 +11,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by cpfort on 8/25/17.
  */
 public class WebDriverFactory {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WebDriverFactory.class);
+
     @Value("${chromeDriver.binary.path}")
     private String chromeDriverBinaryPath;
 
     @Value("${firefox.binary.path}")
     private String firefoxBinaryPath;
-
-    private static final Logger LOG = LoggerFactory.getLogger(WebDriverFactory.class);
 
     public WebDriver webDriver(URL seleniumGridHost, String browser) {
         WebDriver driver = null;
@@ -43,6 +46,9 @@ public class WebDriverFactory {
                 break;
             case FIREFOX:
                 driver = firefoxDriver(seleniumGridHost);
+                break;
+            case IPHONE_6_EMULATOR:
+                driver = mobileEmulationDriver(seleniumGridHost, "Apple iPhone 6");
                 break;
             default:
                 driver = chromeDriver(seleniumGridHost);
@@ -65,9 +71,27 @@ public class WebDriverFactory {
             seleniumGridHost, desiredCapabilities));
     }
 
+    private WebDriver mobileEmulationDriver(URL seleniumGridHost, String mobileEmulatedBrowser) {
+        Map<String, Object> chromeOptions = new HashMap<>();
+        Map<String, String> chromeEmulationOptions = new HashMap<>();
+        chromeEmulationOptions.put("deviceName", mobileEmulatedBrowser);
+        chromeOptions.put("mobileEmulation", chromeEmulationOptions);
+
+        LOG.debug("ChromeOptions: " + chromeOptions);
+        LOG.debug("Chrome emulation options: " + chromeEmulationOptions);
+
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+
+        return (null == seleniumGridHost ?
+            new ChromeDriver(desiredCapabilities) :
+            new RemoteWebDriver(seleniumGridHost, desiredCapabilities));
+    }
+
     public enum BrowserType {
         CHROME,
-        FIREFOX
+        FIREFOX,
+        IPHONE_6_EMULATOR
     }
 
 }
